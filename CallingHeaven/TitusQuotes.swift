46498b0080baed857fdf811392ca36dee522b98f
Laura Paxton <laurapaxton3@gmail.com>
2015-03-24 21:03:26 -0700

    Work in Progress
    Trying to work out constraints for the buttons on the SVC and it’s not
    working yet. Somehow, the Titus files all disappeared again and had to
    be added back in!

---
+++ CallingHeaven/TitusQuotes.swift
@@ -0,0 +1,168 @@
+//
+//  TitusQuotes.swift
+//  CallingHeaven
+//
+//  Created by My Mac on 2/6/15.
+//  Copyright (c) 2015 Carmel Heart Media. All rights reserved.
+//
+
+import Foundation
+
+struct TitusQuotes {
+    
+    let factsArray = [
+"O Jesus, when I look on you my love for you starts up anew, and tells me that your heart loves me, and you my special friend would be.",
+
+"More courage I will need for sure, But any pain I will endure, Because it makes me like to you And leads unto your kingdom too.",
+        
+"In sorrow do I find my bliss, For sorrow now no more is this: Rather the path that must be trod, That makes me one with you, my God.",
+        
+"Oh, leave me here alone and still, And all around the cold and chill. To enter here I will have none; I weary not when I'm alone. For, Jesus you are at my side; Never so close did we abide. Stay with me, Jesus, my delight, Your presence near makes all things right.",
+        
+"Now read, now pray, now work with fervor; so time will pass quickly and work will be easier",
+        
+        "My vocation to the Church and to the priesthood brought me so many grand and beautiful things that I willingly accept something unpleasant in return for it.",
+        
+        "And although I do not know what will become of it, I know myself to be wholly in God's hands. Who will separate us from the love of God?",
+    
+        "True pupils of the school of Carmel should be in a high degree wrapped up in themselves, to find and meet God in the innermost recesses of their souls.",
+        
+    "Carmel is a mountain of peace high above a world filled with chaos.",
+        
+        " I see God in the work of his hands and the marks of his love in every visible thing. I am seized by a supreme joy which is above all other joys.",
+        //10
+        
+        
+        "First of all, we have to see God as the fundamental basis for our being. This basis is hidden in the inner depth of our nature. There we have to see him and to meditate on him.... We then not only adore him in our own being but also in everything that exists.",
+       
+        
+        "O Jesus, let me weep for myself, for I am nothing but dry wood to be cast into the fire. But you give new life to the dry wood by grafting it onto the wood of the cross.",
+    
+        "Suffering is the way to heaven. In the cross is salvation, in the cross is victory. God willed it so. He Himself assumed the obligation of suffering in view of the glory of redemption. St. Paul makes it clear to us how all the disasters of this earthly life are insignificant, how they must be considered as nothing and passing, in comparison with the glory that will be revealed to us when the time of suffering is past and we come to share in God's glory.",
+        
+        "We object when He hands us the chalice of His suffering. It is so difficult for us to resign ourselves to suffering. To rejoice in it strikes us as heroic. What is the value of our offering of self if we unite ourselves each morning only in word and gesture, rather than in thought and will, to that offering which we, together with the Church, make of Him with whom we are in the one body?",
+        
+        "Mary, who kept all God's words in her heart, in the fullness of grace granted her, understood the great value of suffering. While the apostles fled, she went out to meet the Saviour on the way to Calvary and stood beneath the cross, in order to share His grief and shame to the end. And she carried Him to the grave, firmly trusting that He would rise.",
+        
+        "Then the hearts of the disciples burned within them. God's word had set them on fire. And when the Holy Spirit had descended on them to fan that divine fire into flame, then they were glad to suffer scorn and persecution, whereby they resembled Him Who had preceded them on the way of suffering.",
+        
+    "Only charity toward one’s neighbor or obedience can be reasons for leaving God for the sake of God.",
+        
+        "Holy thoughts will protect you. The breastplate which covers the whole body represents justice, a well regulated life, the observance of the commandments and duties of daily life.",
+        
+        "The kingdom of God which is within us — the old comparison of the “the soul’s spark ” — must be extended without interruption or end by occupying us completely.",
+    
+    " In view of God’s riches, let us ask him to enrich us, for in the measure that he gives himself to us, he renders us unceasingly more like to himself. We should collaborate in his action by uniting ourselves ever more intimately to him; and we should forever rejoice over this union with God.",
+        
+        "we must hunger and thirst for God without ceasing and open our mouth to breathe God. We should start by offering ourselves and every creature to God. ",
+    
+        ///21
+        
+        "This fire, which burns in us, sets us aflame, and the flame of our love is united to Divine Love which enflames our heart.",
+        
+        "Contemplation is still a pure gift of God; but it is important that we for our part remove all the obstacles and practice the virtues so that we may be found disposed in the way which God demands before giving his mystical favors.",
+        
+        "And so providentially exterior activity proceeds from union with God but should not interrupt it.",
+        
+        "No doubt it is necessary to preach, study and work, but because of the dangers which exterior activity brings, it is necessary for young scholastics to exercise themselves intensely in the principal object of their vocation and establish themselves solidly in the practice of meditation and contemplation.",
+        
+        " First light breaks in the soul and then in this light the soul sees the source of light. But the soul must have this light before it can see." ,
+    
+        "In the effulgence of its new light the soul becomes keenly aware of its own natural infirmities, but God draws it above itself and in the realization of its own infirmities, the soul understands ever more perfectly the omnipotence of God and His condescending love. In this way, to use St. Paul's words, the soul goes from light to light.",
+        
+        "The imitation of Mary, the most elevated of all creatures, set as an example before us by God Himself, shows Mary as the pattern of all virtues. She is the mirror in which we should ever watch ourselves, the Mother whom her children ought to resemble ever more." ,
+    
+        "We must not only know what we are doing but we must also account for the motives which prompt our deeds, the inclinations to which we are subject when acting and try to find out where they are able to lead us. Secret inclinations are to be revealed before our own minds and above all the end to which they tend should be distinguished. This knowledge of ourselves, of our deepest being, though it is difficult, is absolutely necessary.",
+        
+        "What we intend to do should not be left to the inspiration of the moment but our whole life should be planned beforehand in such a way that we are sure of victory. Many people work and labour and achieve many things which perhaps appear meritorious in the sight of others, whereas they are not keen on searching out what is asked of them for their own welfare and improvement.",
+        
+        " In the knowledge of its union with Him, the soul is so happy that never more should it like to be separated from him. Its faithfulness in that state being tried, the Beloved cements it in His love and celebrates with the soul the spiritual marriage of unbreakable union and of the most intimate intercourse." ,
+    
+        "So the mystical life is a methodical way, an accommodation of the faculties of the soul to the object of knowledge and love. Because God, Who gives happiness and joy, is the highest and most satisfying object of that knowledge and love, so in the method of love, He must rank first.",
+        
+        "That God must rank first follows not only from the surpassingly infinite perfection of the character and nature of the Divine Being in Himself, but also from the dependence upon God of all we know and love.",
+        
+        "God is the Creator and Conservator of all beings and in His workings His finger touches us. But nowhere else is God so near as in ourselves. There is the first place we must try to find and to see Him."  ,
+    
+        " God, so to say, enlarges His creature and raises it to its highest perfection. There is such a gradual development that it should not be too arduous for nature; but at the same time there is such a supreme rise above all powers of nature that only divine grace is able to lift it to those lofty heights, to lead nature to the ideal established by God.",
+        
+        "Yet no matter how much this high perfection goes beyond the power of nature, it is, nonetheless, a true accomplishment of that nature, a realisation of that which is placed in it by God as a possibility, although it can be realised only by His immediate intervention."  ,
+    
+        "To reach this life of union a long way must be traversed. In the beginning, one striving for recollection will see that a heavy fight against nature is necessary; much must die in us in order that God may live in us free and unhindered.",
+        
+        "God's Son has taken on human nature, so that we could realise again the union of our nature with the divine. We should unite ourselves with Christ and in, with and through Him, with the Holy Trinity.",
+        
+        "In the activity of God we should see the continuation of the creation, just as this activity is the continuation and the further revelation of the eternal birth of the Son from the Father and of the Holy Ghost from the Father and the Son.",
+        
+        "The knowledge of the presence of God in us, the indwelling of the Holy Trinity, should again be awakened in our lives. God should again dwell in us, should be born again in us.",
+        
+        "No creature absorbed more purely the divine light that came into this world; no creature gave it back with less blemish or spot and grew more one with God than Our Lady. In the cherished metaphor of St. John of the Cross, Mary appears before our mind's eye as the greatest example of all; nay more, as the first pane of glass without spot, who gave us the light of the world.",
+        
+        "O how happy is the soul who always feels God living in her, and resting in her. She is bound to separate herself from everything, to fly all intercourse with men and to live in the deepest silence so as not to disturb by the least movement or sound, the rest of the Beloved.",
+    
+        "God's light fills us and illumines us, and at the same time makes us shine. We are 'bearers of Christ.' Would that we saw all lights of the world as darkness, to recognise the eternal true light and make it shine through us." ,
+        
+        ///43
+        
+        "Do not yield to hatred. Be patient. We are here in a dark tunnel but we have to go on. At the end, the eternal light is shining for us.",
+        
+        "Time and place are of little importance. Sometimes on earth the flower blooms in all its glory in the garden of God but most often comes only to bud. But in heaven all God's flowers will open in the glory of the Sun. If the good God, like a good gardener, brings some to perfection here, others hereafter, that is His own mysterious choice.",
+        
+        " To unite our will with God's means a continual effort at self-conquest.",
+        
+        "One should not think of the imitation without thinking of the union, nor of the union without the thought of the imitation. Both flow into each other, but in one period the former is more prominent, in another more attention is paid to the latter. One should rather see both trends blended together into one harmonious whole.",
+        
+        "We also have been chosen by the Holy Trinity for a dwelling, to share the privileges which we admire in Mary, but which God is willing to bestow on us also.",
+        
+        "This love for the divine law and the glory of God will in the end bridle our passions and, ever freer and less hampered by our evil inclinations, we shall cleave to God and serve only Him.",
+        
+        " This contemplation should vivify our love for God's laws, should deepen our sense of that same law and our sense of God's grace, so that we may be inclined to do those things that, though not obligatory, yet tend to God's honour and glory and which we ought to do if we truly love God.",
+    
+        "We must keep our conscience spotless so that we always can account for our acts before God. Yet externally we must ever think of leading an exemplary life in the eyes of our neighbours. We have been placed here among our brethren by God to strive together toward the high ideals which He placed before our mind's eye but unless we guard jealously the purity of our conscience, we cannot gather merits internally.",
+        
+        "We must not only know what we are doing but we must also account for the motives which prompt our deeds, the inclinations to which we are subject when acting and try to find out where they are able to lead us. Secret inclinations are to be revealed before our own minds and above all the end to which they tend should be distinguished. This knowledge of ourselves, of our deepest being, though it is difficult, is absolutely necessary." ,
+    
+        "Our successes, as well as our defeats, should be subjects of meditation, so as to evolve at the end the most perfect schemes for success in the campaign of life What we intend to do should not be left to the inspiration of the moment but our whole life should be planned beforehand in such a way that we are sure of victory.",
+        
+        "Many people work and labour and achieve many things which perhaps appear meritorious in the sight of others, whereas they are not keen on searching out what is asked of them for their own welfare and improvement.",
+    
+        "It goes without saying that in those meditations which are the result of the review of our life we should neither undervalue ourselves so that we too easily despair of attaining our goal, nor overrate ourselves and attempt too much. There are hazards on both sides and we are to keep on the middle of the road.",
+        
+        "God, so to say, enlarges His creature and raises it to its highest perfection. There is such a gradual development that it should not be too arduous for nature; but at the same time there is such a supreme rise above all powers of nature that only divine grace is able to lift it to those lofty heights, to lead nature to the ideal established by God. Yet no matter how much this high perfection goes beyond the power of nature, it is, nonetheless, a true accomplishment of that nature, a realisation of that which is placed in it by God as a possibility, although it can be realised only by His immediate intervention.",
+        
+        " The eyes must be rubbed to see clearer and better what God, already in the external mansions, shows to them who have eyes to see and ears to hear. God can and will enthrall and bind the soul. He delights it to rest and slumber in Him but only then when the soul has succeeded in tearing itself away from that which binds it to the world in the external mansions and in placing itself under the mighty rays of the sun which is described as being able to pervade all things coming within its rays.",
+        
+        "To understand what this spreading of God's shadow, or over-shadowing, means, it must be borne in mind that every object throws a shadow according to its own particular shape and outline. So will the shadow thrown by the lamp of God's beauty be another beauty, according to the kind and quality of God's beauty; and so will the shadow spread by the lamp of God's strength be another strength, etc. Or in other and perhaps better words, all these shadows will be God's beauty, God's strength, etc., themselves, but in shadow, because the soul cannot understand perfectly here on earth. But because this shadow so well accords with the essence and the real being of God, indeed because it is God Himself, therefore the soul knows in shadow the exquisite loveliness of God.",
+    
+        
+        "It is reasonable to renounce all your private interests if you receive this treasure; to withdraw from all created things and to hide, secrete yourself in the innermost hiding place of your soul. There you will shut the door, that is, withdraw you will from all created things and pray to your Father in secret. Thus hidden with your Bridegroom you will feel His presence in secret, enjoy and caress Him in secret and rejoice with Him in being secret, i.e., beyond everything the senses can reach and tongue can express.",
+        
+        "Now then, lovely soul, now you know that the lover you seek is hidden in your soul; be diligent to remain in secret with Him and you will feel Him and embrace Him with the most tender love.",
+        
+        "Through the Incarnation He gave these a supernatural existence and lifted them up, together with man, to the glory of God. Through the glory of the Incarnation of His Son and His resurrection according to the flesh the Father has not only ennobled all creatures, but clothed them also with beauty and dignity. Contemplating this secret, the soul is wounded by love.",
+    
+        "God's light fills us and illumines us, and at the same time makes us shine. We are 'bearers of Christ.' Would that we saw all lights of the world as darkness, to recognise the eternal true light and make it shine through us.",
+        
+        "It is not simply a loving dialogue; that is only the beginning and start. It is a soaring to God, the bursting forth of a flame out of our loving and glowing hearts. It is an attempt, repeated again and again, to unite ourselves as closely as possible to God, or rather, to reform ourselves in God and conform ourselves to Him."  ,
+    
+        "It is an impulse, a desire to lose ourselves in God and God does not repulse us. He takes us to Him and we grow into one spirit, we are filled with His spirit, we live his life. How remarkable! ",
+        
+        "We long for God because we are filled with His spirit, with Himself. And because we are filled with Him, we desire ever more to be filled; we seek Him and so He fills us ever more. This practice transcends all understanding, it transcends all display of affection, it strives immediately to God and aims at nothing else than being one with Him.",
+        
+        "In the innermost, deepest, most essential part of us God is the being of our being, life of our life, the reason of our existence and of everything we do and are able to do. There God is like a spark in our soul. He has kindled fire in us -- fire that imparts light and warmth, fire that must flame up.",
+        
+        //66
+    
+    
+        ]
+    
+    func randomFact() -> String {
+        var unsignedArrayCount = UInt32(factsArray.count)
+        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        var randomNumber = Int(unsignedRandomNumber)
+        
+        return factsArray[randomNumber]
+    }
+    
+}
+
