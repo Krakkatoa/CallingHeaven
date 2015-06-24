e36e2b9f5ab33169418a9943b5d11d8959fa4ef2
Laura Paxton <laurapaxton3@gmail.com>
2015-06-24 15:49:13 -0700

    Connected Code Add-On VC
    Connected the Add-on VC constraints to code and had to adjust
    Autolayout. Now works fine on all screens.

--- Add-onsViewController.swift
+++ Add-onsViewController.swift
@@ -8,43 +8,61 @@
 
 import UIKit
 
+protocol Add_onsViewControllerDelegate{
+    
+}
+
 class Add_onsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+   
         // Do any additional setup after loading the view.
         
         
-    }
+    
    
-
-    @IBOutlet weak var backButton: UIButton!
-    @IBAction func backAction(sender: AnyObject) {
-  self.dismissViewControllerAnimated(true) { () -> Void in }
-        
-        
+@IBOutlet weak var creditsAction: UIButton!
     
+    @IBOutlet var horizontalSpacingConstraints: [NSLayoutConstraint]!
+    
+    @IBOutlet var verticalSpacingConstraints: [NSLayoutConstraint] = []
+
+    @IBOutlet weak var backAction: UIButton!
+   
+    @IBAction func backAction(sender: AnyObject){
+        self.dismissViewControllerAnimated(true) { () -> Void in
+        }
+        
+
+            let screenSize = UIScreen.mainScreen().bounds.size
+            let verticalCoef = screenSize.height / 320.0
+            let horizontalCoef = screenSize.width / 568.0
+            
+            for constraint in verticalSpacingConstraints {
+                constraint.constant *= verticalCoef
+            }
+            
+            for constraint in horizontalSpacingConstraints {
+                constraint.constant *= horizontalCoef
+            }
+
+      
+        
+    var delegate:Add_onsViewControllerDelegate? = nil
+
+    // Dispose of any resources that can be recreated.
+
+
+
+
+
+        
     }
 
-    
-        
-        
-        
-        
-    
-    
-        override func didReceiveMemoryWarning()
-        {
+}
 
-        
-        
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
+
+/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -54,4 +72,4 @@ class Add_onsViewController: UIViewController {
     }
     */
 
-}
+
