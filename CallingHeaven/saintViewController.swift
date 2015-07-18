4e800774b628a8fe535b3cf5a967098418693a9b
Laura Paxton <laurapaxton3@gmail.com>
2015-07-17 17:56:47 -0700

    Revert "only committing to revert"
    This reverts commit ece4bf335b753bcb9879870589a1444def14ba1b.

--- CallingHeaven/saintViewController.swift
+++ CallingHeaven/saintViewController.swift
@@ -20,9 +20,9 @@ class saintViewController: UIViewController {
     var lifeTemporal:String!
     var temporalImage:UIImage!
     var quotesTemporal:String!
+    
+    
     @IBOutlet var testLabel: UITextView!
-    
-    
     @IBOutlet var saintImage: UIImageView!
     
     @IBOutlet weak var journalButton: UIButton!
@@ -116,11 +116,6 @@ class saintViewController: UIViewController {
     let andesQuotes = AndesQuotes ()
     let titusQuotes = TitusQuotes ()
     
-    
-    
-    
-    
-    
     @IBOutlet var horizontalSpacingConstraints: [NSLayoutConstraint]!
     
     @IBOutlet var verticalSpacingConstraints: [NSLayoutConstraint]!
@@ -134,7 +129,7 @@ class saintViewController: UIViewController {
         saintImage.image = temporalImage
         bioLabel2.text = lifeTemporal
         bioLabel3.text = quotesTemporal
-            
+        
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
