cbac92b92608ae2653a2d6ed4a01cf15583c31d8
Laura Paxton <laurapaxton3@gmail.com>
2015-07-21 02:45:46 -0700

    New Designs Linked Into Storyboard
    Fixed Andes button problem again. Re-designed icon, which still needs
    to be re-sized. Designed a help screen and linked it to 3 VCs. The
    question buttons stretch though and I don’t know why right now. Will
    have to work on it tomorrow.

---
+++ CallingHeaven/HelpScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HelpScreenViewController.swift
+//  
+//
+//  Created by My Mac on 7/21/15.
+//
+//
+import UIKit
+
+
+class HelpScreenViewController: UIViewController {
+    
+    
+    @IBOutlet weak var homeButton: UIButton!
+    
+    @IBAction func backAction(sender: AnyObject){
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+
+}
---
+++ HelpScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HelpScreenViewController.swift
+//  
+//
+//  Created by My Mac on 7/21/15.
+//
+//
+
+import Cocoa
+
+class HelpScreenViewController: UIViewController {
+
+}
