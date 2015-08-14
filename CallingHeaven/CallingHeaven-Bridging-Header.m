a78608fd85c35ff401a3d0e025b357e7b8c87627
Laura Paxton <laurapaxton3@gmail.com>
2015-08-14 14:58:28 -0700

    Same
    “

---
+++ CallingHeaven/CallingHeaven-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  callingHeaven-Bridging-Header.m
+//  CallingHeaven
+//
+//  Created by My Mac on 8/14/15.
+//  Copyright (c) 2015 Carmel Heart Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
