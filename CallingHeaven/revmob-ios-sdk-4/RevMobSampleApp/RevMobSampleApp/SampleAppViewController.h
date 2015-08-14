f8c7877c9d22c993f2a46971a7ff7691b56a3cc1
Laura Paxton <laurapaxton3@gmail.com>
2015-08-14 15:16:22 -0700

    Revert 272a93b..ec7f1aa
    This rolls back to commit 272a93bb0bdeb05cf95c12f8e15da168bce3b146.

--- CallingHeaven/revmob-ios-sdk-4/RevMobSampleApp/RevMobSampleApp/SampleAppViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <RevMobAds/RevMobAds.h>
-#import <RevMobAds/RevMobAdsDelegate.h>
-
-@interface SampleAppViewController : UIViewController <RevMobAdsDelegate>
-
-@end
